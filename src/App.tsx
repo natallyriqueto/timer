9ffb3069058079e02191e33b714d1bf23655e0fd
Natally <natallyriqueto@hotmail.com>
2023-06-05 19:25:34 -0400

    add header style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { Button } from './components/Button'
 import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from './components/styles/themes/default'
-import { GlobalStyle } from './components/styles/global'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
