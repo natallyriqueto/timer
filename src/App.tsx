387bff6a994ddd3ca65f1640f705e6723f5ba87f
Natally <natallyriqueto@hotmail.com>
2023-06-05 18:17:06 -0400

    add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { Button } from "./components/Button";
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./components/styles/themes/default";
-import { GlobalStyle } from "./components/styles/global";
+import { Button } from './components/Button'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './components/styles/themes/default'
+import { GlobalStyle } from './components/styles/global'
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"/>
+      <Button variant="primary" />
 
       <GlobalStyle />
     </ThemeProvider>
