b5db9555c36d2cc7e1a22bf8397ae41926049823
Natally <natallyriqueto@hotmail.com>
2023-06-05 23:40:34 -0400

    finishing history component css

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,6 +1,47 @@
+import { HistoryContainer, HistoryList } from "./styles";
 
 export function History() {
     return (
-        <h1>oi</h1>
+        <HistoryContainer>
+            <h1>My history</h1>
+            <HistoryList>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Task</th>
+                            <th>Duration</th>
+                            <th>Beginning</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>My first task</td>
+                            <td>20 minutes</td>
+                            <td>2 months ago</td>
+                            <td>Done</td>
+                        </tr>
+                        <tr>
+                            <td>My first task</td>
+                            <td>20 minutes</td>
+                            <td>2 months ago</td>
+                            <td>Done</td>
+                        </tr>
+                        <tr>
+                            <td>My first task</td>
+                            <td>20 minutes</td>
+                            <td>2 months ago</td>
+                            <td>Done</td>
+                        </tr>
+                        <tr>
+                            <td>My first task</td>
+                            <td>20 minutes</td>
+                            <td>2 months ago</td>
+                            <td>Done</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </HistoryList>
+        </HistoryContainer>
     );
 }
