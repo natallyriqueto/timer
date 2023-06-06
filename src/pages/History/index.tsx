82a27ec10f4273f71e5bf2c11f8b73d14e479f85
Natally <natallyriqueto@hotmail.com>
2023-06-05 23:56:34 -0400

    creating component status inside the table

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
     return (
@@ -19,25 +19,33 @@ export function History() {
                             <td>My first task</td>
                             <td>20 minutes</td>
                             <td>2 months ago</td>
-                            <td>Done</td>
+                            <td>
+                                <Status statusColor="yellow">Done</Status>
+                            </td>                        
                         </tr>
                         <tr>
                             <td>My first task</td>
                             <td>20 minutes</td>
                             <td>2 months ago</td>
-                            <td>Done</td>
+                            <td>
+                                <Status statusColor="green">Done</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>My first task</td>
                             <td>20 minutes</td>
                             <td>2 months ago</td>
-                            <td>Done</td>
+                            <td>
+                                <Status statusColor="red">Done</Status>
+                            </td>                        
                         </tr>
                         <tr>
                             <td>My first task</td>
                             <td>20 minutes</td>
                             <td>2 months ago</td>
-                            <td>Done</td>
+                            <td>
+                                <Status statusColor="red">Done</Status>
+                            </td>                        
                         </tr>
                     </tbody>
                 </table>
